0b840b3d60e506af441ce7de82225e8c05f9d8a2
Ajay Krishnan <ajaykrishnan@gogosoon.com>
2020-10-22 11:31:23 +0530

    Updates

---
+++ precache-manifest.0578f39ce8cf7a970a61c2d001a7bf94.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7435a1c4a4277d0a8a7f40cfc0eac15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5723933194afc341830",
+    "url": "/static/css/2.3a9a1458.chunk.css"
+  },
+  {
+    "revision": "f1a9c14ce54308afb2a7",
+    "url": "/static/css/main.27ad2bf0.chunk.css"
+  },
+  {
+    "revision": "c5723933194afc341830",
+    "url": "/static/js/2.084f3693.chunk.js"
+  },
+  {
+    "revision": "19651e253f4ad91aeb8014a4b6ce739d",
+    "url": "/static/js/2.084f3693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a9c14ce54308afb2a7",
+    "url": "/static/js/main.710ef96d.chunk.js"
+  },
+  {
+    "revision": "206755f215b9f5a5253d",
+    "url": "/static/js/runtime-main.c63cf739.js"
+  },
+  {
+    "revision": "588bbe79963afd5f29de9d73754c766e",
+    "url": "/static/media/Raaj.588bbe79.png"
+  },
+  {
+    "revision": "1d6a445ba0f442973d4c1f2acd3dafa9",
+    "url": "/static/media/Sangili.1d6a445b.png"
+  },
+  {
+    "revision": "6553e5943f90eb5d7ee8a318eb781b8b",
+    "url": "/static/media/Zit Logo.6553e594.png"
+  },
+  {
+    "revision": "444c62687546b5fccd2573ff711a605d",
+    "url": "/static/media/Zit.444c6268.mp4"
+  },
+  {
+    "revision": "a034d70ed6f7e6cfd2c049b6b4961c08",
+    "url": "/static/media/Zit_Banner_Logo.a034d70e.jpg"
+  },
+  {
+    "revision": "8df1865be38f91cd41680cd4a65ddaa0",
+    "url": "/static/media/Zit_banner_Square.8df1865b.png"
+  },
+  {
+    "revision": "48e5843ec05617e72e1cca1d6e63519a",
+    "url": "/static/media/ajay.48e5843e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/zitwebsite/precache-manifest.5e2a89b86fa9caef79ca1664a0b6bef4.js"
+  "/precache-manifest.0578f39ce8cf7a970a61c2d001a7bf94.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zitwebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
